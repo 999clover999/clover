03d0962aef4ce1b055af17927450688100779b53
999clover999 <30555263+999clover999@users.noreply.github.com>
2017-08-03 01:39:42 +0900

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import os
 
 def catalogSpider():
     var2 = raw_input('Please enter a name of board with / /: ')
-    var3 = raw_input('Please enter an ID of thread, the numbers at the end of the thread: ')
+    var3 = raw_input('Please enter thred ID: ')
     kek = 'https://boards.4chan.org' + str(var2)
     url = str(kek) + 'thread/' + str(var3)
     source_code = requests.get(url)
@@ -16,14 +16,15 @@ def catalogSpider():
     for link in soup.findAll('a', {'class': 'fileThumb'}):
         href = 'https:' + link.get('href')
         hreff = href[-10:]
-        print hreff
         print href
         name = random.randrange(1, 1000000)
         full_name = str(name) + hreff
+        print full_name
         urllib.urlretrieve(href, full_name)
 
 
-catalogSpider()
-raw_input("press ENTER to close")
+topkek = True
+while topkek == True:
+ catalogSpider()
 
 
